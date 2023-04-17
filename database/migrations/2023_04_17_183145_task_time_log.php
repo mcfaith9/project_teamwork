d1f94f2bde0e9e64ee9b5ecff154a6db9d77969d
mcfaith9 <marclouie.cabigas@yahoo.com>
2023-04-18 02:38:30 +0800

    make migration for task_time_log and drop ->text('assignee_id')->nullable(); in task

---
+++ database/migrations/2023_04_17_183145_task_time_log.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_time_log', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('task_id');
+            $table->unsignedBigInteger('user_id');
+            $table->integer('prev_time_today')->nullable();  
+            $table->integer('time_log')->nullable();
+            $table->timestamps();         
+            
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_time_log');
+    }
+};
