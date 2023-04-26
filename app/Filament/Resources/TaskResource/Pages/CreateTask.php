0b33fbe9a7830681cd6a95c0d65a49c795983521
mcfaith9 <marclouie.cabigas@yahoo.com>
2023-04-26 11:12:02 +0800

    added shield to page hints

--- app/Filament/Resources/TaskResource/Pages/CreateTask.php
+++ app/Filament/Resources/TaskResource/Pages/CreateTask.php
@@ -5,8 +5,11 @@ namespace App\Filament\Resources\TaskResource\Pages;
 use App\Filament\Resources\TaskResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 
 class CreateTask extends CreateRecord
 {
+    use HasPageShield;
+    
     protected static string $resource = TaskResource::class;
 }
